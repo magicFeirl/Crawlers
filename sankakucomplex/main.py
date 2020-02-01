10b48a128e2f937954344879501540bec66434b5
magicFeirl <2100709458@qq.com>
2020-02-01 23:08:33 +0800

    更新代码

--- sankakucomplex/main.py
+++ sankakucomplex/main.py
@@ -8,7 +8,7 @@ from downloader import Downloader
 async def main():
     '''在此处设置下载参数'''
 
-    d = Downloader('tags_name', 'dir_name', 1, 2)
+    d = Downloader('cookie(touhou)', 'c ', 1, 2)
 
     await d.start()
 
