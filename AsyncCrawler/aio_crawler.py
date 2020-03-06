8f412f1e88aa4be169bfa9edfffb6003b08f9b62
magicFeirl <2100709458@qq.com>
2020-03-06 11:59:15 +0800

    Wheel

--- AsyncCrawler/aio_crawler.py
+++ AsyncCrawler/aio_crawler.py
@@ -8,24 +8,22 @@ aiofiles
 import asyncio
 import time
 
+
 class AsyncCrawler():
 
-    def __init__(self, session, url_list, coro_num, callback, \
-    consume_num, consume_callback):
-        '''callback 为处理请求队列返回数据的回调函数，须返回一个list
-        consum_callback 下载回调'''
+    def __init__(self, session, url_list, request_callback, \
+    download_callback, is_clear_queue=True):
+        '''request_callback 为处理请求队列返回数据的回调函数，须返回一个list
+        download_callback 下载回调'''
 
         self.session = session
         self.url_list = url_list
-        self.coro_num = coro_num
-        self.callback = callback
-
-        self.consume_num = consume_num
-        self.consume_callback = consume_callback
+        self.request_callback = request_callback
+        self.download_callback = download_callback
+        self.is_clear_queue = is_clear_queue
 
         self.url_queue = asyncio.Queue() # 请求队列
-        self.consume_url_queue = asyncio.Queue() # 下载队列
-        self.cq = True
+        self.download_url_queue = asyncio.Queue() # 下载队列
 
         self.init_queue()
 
@@ -50,15 +48,15 @@ class AsyncCrawler():
         可以通过设置run方法的clear_queue参数来取消默认行为。'''
 
         while True:
-            url =  await self.url_queue.get()
+            url = await self.url_queue.get()
 
             try:
                 async with self.session.get(url) as resp:
-                    result = await self.callback(await resp.text())
-                    if len(result):
+                    result = await self.request_callback(await resp.text())
+                    if len(result) != 0:
                         for i in result:
-                            await self.consume_url_queue.put(i)
-                    elif self.cq:
+                            await self.download_url_queue.put(i)
+                    elif self.is_clear_queue:
                         # print('获取到空列表，清空队列')
                         await self.clear_queue()
 
@@ -72,45 +70,48 @@ class AsyncCrawler():
             finally:
                 self.url_queue.task_done()
 
-    async def consume(self):
+    async def download(self):
         '''一个简单的消费者协程'''
 
         while True:
-            url = await self.consume_url_queue.get()
+            url = await self.download_url_queue.get()
             try:
-                await self.consume_callback(url, self.session)
+                await self.download_callback(url, self.session)
+            except asyncio.TimeoutError:
+                print(f'Download {url} timeout')
+            except asyncio.CancelledError:
+                raise
             except Exception as exce:
-                print(f'Consume Url Error:', str(exce))
+                print(f'Download Url Error:', str(exce))
                 print(f'Error Type: {type(exce)}')
             finally:
-                self.consume_url_queue.task_done()
+                self.download_url_queue.task_done()
 
-    async def run(self, show_msg=True, clear_queue=True):
+    async def run(self, request_coro, download_coro, display_time=True):
         '''启动请求和下载协程，阻塞至队列任务结束
         show_msg -> boolean 是否显示耗时
         clear_queue -> boolean 当请求队列获取到空列表数据时是否清空请求队列'''
 
         tasks = []
-        self.cq = clear_queue
-
         start_time = time.time()
-        for i in range(self.consume_num):
-            task = asyncio.create_task(self.consume())
+
+        for _ in range(download_coro):
+            task = asyncio.create_task(self.download())
             tasks.append(task)
             await asyncio.sleep(0.0001) # 确保数据在加入队列时有序
 
-        for i in range(self.coro_num):
+        for _ in range(request_coro):
             task = asyncio.create_task(self.get())
             tasks.append(task)
             await asyncio.sleep(0.0001)
 
         await self.url_queue.join()
-        await self.consume_url_queue.join()
+        await self.download_url_queue.join()
 
         for task in tasks:
             task.cancel()
 
         await asyncio.gather(*tasks, return_exceptions=True)
 
-        if show_msg:
-            print(f'完毕。耗时 {round(time.time() - start_time, 2)} s.')
+        if display_time:
+            print(f'完毕。耗时 {round(time.time() - start_time, 3)} s.')
