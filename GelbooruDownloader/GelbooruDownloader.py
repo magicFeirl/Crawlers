4014c5a693b0565c58171562e503d2a24688f7eb
magicFeirl <2100709458@qq.com>
2020-06-22 16:56:15 +0800

    API 版图片爬虫

--- GelbooruDownloader/GelbooruDownloader.py
+++ GelbooruDownloader/GelbooruDownloader.py
@@ -2,48 +2,34 @@ import os
 import time
 import asyncio
 
-from lxml import etree
+from lxml import html
 
 from crawler_utils import Downloader
 from crawler_utils import ClientConfig
 
-# //span[@class="thumb"]/a/img/@src
-
-# 页数增长规律:
-# 第一页从 0 开始，后一页为前一页页数+42
-# 页数过大无法获取结果，目测页数在 400(16800) 页以内均可获得结果
-# 当页数非 42 的倍数时页数会被除以 42 向下取整（目测）
-# 当解析链接列表为空或超过用户页数限制时停止队列
-# Gelbooru 的图片链接可以通过 IDM 下载，因此考虑把链接保存为文本
-# 每页最多显示 42 张图
-# 传入 Cookie: fringeBenefits=yup 可以下载所有图片（甚至不需要PHPSESSID...)
-# 图片后缀不同会导致 404，可以用 head 请求参看图片类型，这里只下载 sample 质量的图片，后缀目测一致，且大小较小
-# self.file_url = 'https://img2.gelbooru.com/images'
-
-# 几十行代码摸了一上午...
-
-# 发现页面有类的图片标签就是图片元素
-# 以前的 xpath 写的太复杂了...
-XPATH = '//img[@class]/@src'
-
+# API 的 pid 下标是从 0 开始的
 
 class GelBooruDownloader(Downloader):
     RATING_DICT = dict(a='', s='+rating:safe',
     q='+rating:questionalbe', e='+rating:explicit')
 
     HEADERS = {
-        'User-Agent': 'wasp', 'Cookie': 'fringeBenefits=yup',
+        'User-Agent': 'wasp',
+        'Cookie': 'fringeBenefits=yup',
         'referer': 'https://gelbooru.com'
     }
 
-    def __init__(self, tags, destfile=None, begin=1, end=1, rating='e', max_connect_num=3):
+    def __init__(self, tags, destfile=None, begin=0, end=1,
+    rating='e', max_connect_num=3, limit=40):
         ccf = ClientConfig(headers=self.HEADERS, max_connect_num=max_connect_num)
 
         super(GelBooruDownloader, self).__init__(ccf=ccf, req_download=False)
 
         self.tags = tags
+        self.limit = limit
 
         self.init_output(destfile)
+
         self.init_connect_queue(self.init_urls(begin, end, rating))
         # print(self.connect_queue)
 
@@ -61,44 +47,47 @@ class GelBooruDownloader(Downloader):
             self.output = open(destfile, 'w')
 
     def init_urls(self, begin, end, rating):
-        begin= (begin - 1) * 42
-        end = end * 42
+        API = 'https://gelbooru.com/index.php?page=dapi&s=post&q=index&'+ \
+        'pid={}&tags={}{}&limit={}'
 
-        return ['https://gelbooru.com/index.php?page=post&s=list&'
-        'tags={}{}&pid={}'.format(self.tags,
-        self.RATING_DICT.get(rating, '+rating:explicit'), pid)
-        for pid in range(begin, end, 42)]
+        return [API.format(pid, self.tags,
+        self.RATING_DICT.get(rating, '+rating:explicit'), self.limit)
+        for pid in range(begin, end)]
 
     async def connect_callback(self, response):
         status = response.status
         if status == 200:
-            thumb_imgs = self.parse_html(await response.text(), XPATH)
-            # print(thumb_imgs)
-            if thumb_imgs:
-                for img in thumb_imgs:
+            print(f'{response.url}')
 
-                    await self.download_queue.put(img)
+            xpath = '//post/@sample_url' # 只下载 sample 质量的文件
+            # 这里如果直接传 text lxml 会报 ValueError，貌似是因为 Unicode 的原因
+            html = await response.read()
+            urls = self.parse_html(html, xpath)
+            # print(html)
+            for url in urls:
+                await self.download_queue.put(url)
             else:
                 await self.clear_connect_queue()
         else:
             print(f'请求网页 HTTP 状态码错误:{status}')
 
     async def download_callback(self, url):
-        # print(url)
-        print(url.replace('thumbnail', 'sample'), file=self.output)
+        print(url, file=self.output)
+
+    def parse_html(self, text, xpath):
+        selector = html.fromstring(text)
+        return selector.xpath(xpath)
 
     def clear(self):
         if self.output:
             self.output.flush()
             self.output.close()
 
-    def parse_html(self, text, xpath):
-        selector = etree.HTML(text)
-        return selector.xpath(xpath)
-
 
 async def main():
-    gbd = GelBooruDownloader('touhou', destfile='t', rating='a', max_connect_num=1)
+    gbd = GelBooruDownloader('ke-ta+uncensored', destfile='keta_e_s',
+    rating='e', max_connect_num=5, end=5)
+
     await gbd.start()
     gbd.clear()
 
